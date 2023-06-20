7e2c5947536be9568736d1d246421f1903e79deb
jonghyukm <128242790+jonghyukm@users.noreply.github.com>
2023-06-20 21:47:42 +0900

    Delete Finding_Roots directory

--- Finding_Roots/Group3_홍종혁_Problem#5.py
+++
@@ -1,9 +0,0 @@
-#Problem5
-x = float(input("Input any positive real number : "))    #양의 실수 x를 입력받음
-p = 1;q=1    #초기값 설정
-while 1:    #while문으로 무한 반복 수행
-    p = (q + x/q)/2    # Newton square-root algorithm 식
-    if q==p:break    # q와 p가 같을 시 반복문 종료
-    q = p    #q에 p 저장
-print(f'approximation of root of x is {p}')  #p를 출력한다. 이는 square root of x의 근사값이다.
-print(f'√x = {x**0.5}')    #실제 값과 비교하기 위해 실제 값 출력
